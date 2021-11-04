b1010b5528b2f603fe68864b141b4b8b89c57516
greinisch <greinisch@youwol.com>
2021-11-04 17:26:01 +0100

    :art: re-format code

--- src/lib/modal.view.ts
+++ src/lib/modal.view.ts
@@ -6,8 +6,8 @@ export namespace Modal {
     export class State {
 
         constructor(public readonly ok$: Subject<MouseEvent> = new Subject<MouseEvent>(),
-                    public readonly cancel$: Subject<MouseEvent> = new Subject<MouseEvent>(),
-                    public readonly signals:{[key:string]: Subject<MouseEvent>} = undefined) {
+            public readonly cancel$: Subject<MouseEvent | KeyboardEvent> = new Subject<MouseEvent | KeyboardEvent>(),
+            public readonly signals: { [key: string]: Subject<MouseEvent> } = undefined) {
         }
     }
 
@@ -15,26 +15,29 @@ export namespace Modal {
     export class View implements VirtualDOM {
 
         public readonly state: State
-        
+
         static defaultStyle = {
-            position:'absolute', 
-            top:'0px', 
-            left: '0px', 
-            'z-index':100, 
-            width:'100vw', 
-            height:'100vh', 
+            position: 'absolute',
+            top: '0px',
+            left: '0px',
+            'z-index': 100,
+            width: '100vw',
+            height: '100vh',
             'background-color': 'rgba(0,0,0,0.5)'
         }
-        static defaultClass  = 'd-flex justify-content-around'
+        static defaultClass = 'd-flex justify-content-around'
 
         public readonly tag = 'div'
         public readonly class: string
-        public readonly style: {[key:string]: any}
+        public readonly style: { [key: string]: any }
 
-        public readonly onclick = (ev: MouseEvent) => this.state.cancel$.next(ev)
+        public readonly onclick = (ev: MouseEvent) => {
+            this.state.cancel$.next(ev)
+        }
 
-        public readonly children : [VirtualDOM]
-        constructor( { state, contentView, ...rest } : { state: State, contentView: (State)=>VirtualDOM }) {
+        public readonly children: [VirtualDOM]
+
+        constructor({ state, contentView, ...rest }: { state: State, contentView: (State) => VirtualDOM }) {
 
             Object.assign(this, rest)
             document.onkeydown = (ev: KeyboardEvent) => {
