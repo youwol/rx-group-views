3e7ebd024726d12c628410c3c4c3cf5e8e3d2814
youwol-guillaume <greinisch@youwol.com>
2022-09-12 16:06:15 +0200

    :wrench: update template.py + config files

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-Tiny library to render HTML documents using reactive programing primitives.
+# @youwol/fv-group
+
+Grouping widgets using flux-view
 
 This library is part of the hybrid cloud/local ecosystem 
-[YouWol](https://platform.youwol.com/applications/@youwol/platform/latest)
+[YouWol](https://platform.youwol.com/applications/@youwol/platform/latest).
 
 ## Links
 
---
+++ src/auto-generated.ts
@@ -0,0 +1,26 @@
+
+const runTimeDependencies = {
+    "load": {
+        "rxjs": "^6.5.5",
+        "@youwol/flux-view": "^1.0.0"
+    },
+    "differed": {},
+    "includedInBundle": []
+}
+const externals = {
+    "rxjs": "rxjs_APIv6",
+    "@youwol/flux-view": "@youwol/flux-view_APIv1"
+}
+export const setup = {
+    name:'@youwol/fv-group',
+    assetId:'QHlvdXdvbC9mdi1ncm91cA==',
+    version:'0.2.0',
+    shortDescription:"Grouping widgets using flux-view",
+    developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/fv-group',
+    npmPackage:'https://www.npmjs.com/package/@youwol/fv-group',
+    sourceGithub:'https://github.com/youwol/fv-group',
+    userGuide:'https://l.youwol.com/doc/@youwol/fv-group',
+    apiVersion:'02',
+    runTimeDependencies,
+    externals
+}
