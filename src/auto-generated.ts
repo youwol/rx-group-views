69a6be38f47e1f0d8471f4249d9d37a19ea5bed6
youwol-guillaume <greinisch@youwol.com>
2022-09-14 06:48:58 +0200

    :bookmark: 0.2.1 - pipeline updated

--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -2,19 +2,37 @@
 const runTimeDependencies = {
     "load": {
         "rxjs": "^6.5.5",
-        "@youwol/flux-view": "^1.0.0"
+        "@youwol/flux-view": "^1.0.3"
     },
     "differed": {},
     "includedInBundle": []
 }
 const externals = {
-    "rxjs": "rxjs_APIv6",
-    "@youwol/flux-view": "@youwol/flux-view_APIv1"
+    "rxjs": {
+        "commonjs": "rxjs",
+        "commonjs2": "rxjs",
+        "root": "rxjs_APIv6"
+    },
+    "@youwol/flux-view": {
+        "commonjs": "@youwol/flux-view",
+        "commonjs2": "@youwol/flux-view",
+        "root": "@youwol/flux-view_APIv1"
+    }
+}
+const exportedSymbols = {
+    "rxjs": {
+        "apiKey": "6",
+        "exportedSymbol": "rxjs"
+    },
+    "@youwol/flux-view": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/flux-view"
+    }
 }
 export const setup = {
     name:'@youwol/fv-group',
-    assetId:'QHlvdXdvbC9mdi1ncm91cA==',
-    version:'0.2.0',
+        assetId:'QHlvdXdvbC9mdi1ncm91cA==',
+    version:'0.2.1',
     shortDescription:"Grouping widgets using flux-view",
     developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/fv-group',
     npmPackage:'https://www.npmjs.com/package/@youwol/fv-group',
@@ -22,5 +40,9 @@ export const setup = {
     userGuide:'https://l.youwol.com/doc/@youwol/fv-group',
     apiVersion:'02',
     runTimeDependencies,
-    externals
+    externals,
+    exportedSymbols,
+    getDependencySymbolExported: (module:string) => {
+        return `${exportedSymbols[module].exportedSymbol}_APIv${exportedSymbols[module].apiKey}`
+    }
 }
